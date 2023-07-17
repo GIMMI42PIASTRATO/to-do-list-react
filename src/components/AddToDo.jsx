7fa8fec8d18cb21d4c597843d9832e3deddad395
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-07-17 11:04:35 +0200

    Started creating the AddToDo bar

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import style from "../style/AddToDo.module.css";
+import { TbPlus, TbPumpkinScary } from "react-icons/tb";
 
 export default function AddToDo() {
     return (
-        <div>
-            <input type="text" placeholder="Add a ToDo" />
+        <div className={style.container}>
+            <TbPlus />
+            <input type="text" placeholder="New task" className={style.addToDo} />
         </div>
     );
 }
